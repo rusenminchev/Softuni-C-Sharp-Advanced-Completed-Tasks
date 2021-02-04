fe5c95c68de753558008c2265db94f4c77cc7510
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:38:42 +0200

    add

---
+++ 02. Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] clothesInTheBox = Console.ReadLine().Split().Select(byte.Parse).ToArray();
+
+            Stack<byte> clothesValue = new Stack<byte>(clothesInTheBox);
+
+            byte racsCapacity = byte.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int racsCounter = 1;
+
+            while (clothesValue.Count > 0)
+            {
+
+                sum += clothesValue.Peek();
+
+                if (racsCapacity >= sum)
+                {
+                    clothesValue.Pop();
+                }
+                else
+                {
+                    racsCounter++;
+                    sum = 0;
+                }
+            }
+
+            Console.WriteLine(racsCounter);
+
+        }
+    }
+}
