51ac3852b16f2c5dc087e6b510eefb696e18a6a9
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:39:04 +0200

    add

---
+++ 02. Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_9.__Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+
+            string input = string.Empty;
+            Stack<string> undoList = new Stack<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string cmd = cmdArgs[0];
+
+                if (cmd == "1")
+                {
+                    undoList.Push(input);
+                    input = input + cmdArgs[1];
+
+                }
+                else if (cmd == "2")
+                {
+                    int count = int.Parse(cmdArgs[1]);
+                    undoList.Push(input);
+
+
+                    if (input.Length >= count)
+                    {
+                        input = input.Remove(input.Length - int.Parse(cmdArgs[1]), int.Parse(cmdArgs[1]));
+                    }
+                }
+                else if (cmd == "3")
+                {
+
+                    Console.WriteLine(input[int.Parse(cmdArgs[1]) - 1]);
+
+                }
+                else if (cmd == "4")
+                {
+                    if (undoList.Count > 0)
+                    {
+                        input = undoList.Pop();
+                    }
+                    else
+                    {
+                        input = string.Empty;
+                    }
+                }
+            }
+        }
+    }
+}
