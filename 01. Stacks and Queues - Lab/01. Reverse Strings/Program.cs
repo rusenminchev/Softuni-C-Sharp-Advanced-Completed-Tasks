92e72ea1d8948838aeacf82dab631ce2fedc5b60
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:34:26 +0200

    add

---
+++ 01. Stacks and Queues - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+
+namespace Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stack = new Stack<char>(Console.ReadLine());
+
+            while (stack.Any())
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
