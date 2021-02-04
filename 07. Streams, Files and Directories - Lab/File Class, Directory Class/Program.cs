aae219ec63654e4ebf1c37524c0de5f43a56fec5
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:45:19 +0200

    add

---
+++ 07. Streams, Files and Directories - Lab/File Class, Directory Class/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace File_Class__Directory_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Чрез class File, можем автоматично да пишем/четем и други. Лесно и бързо.
+
+            var text = File.ReadAllText("TextFile1.txt");
+
+
+            File.WriteAllText("Output.txt", text);
+
+            ----
+
+            Directory.CreateDirectory("Test Directory");
+
+            Directory.Delete("Test Directory, true);
+            Directory.Move("Test", "New Directory");
+
+            Directory.GetFiles();
+
+            string[] fileInDirectory = Directory.GetFiles("Test Directory");
+
+            Directory.GetDirectories();
+            string[] subDirectories = Directory.GetFiles("Test Directory");
+        }
+    }
+}
