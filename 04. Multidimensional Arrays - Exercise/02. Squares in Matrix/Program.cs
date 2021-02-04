73207b56b0f3f7018237b0e41e372720aa3167f4
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:41:15 +0200

    add

---
+++ 04. Multidimensional Arrays - Exercise/02. Squares in Matrix/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_2._Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int rows = n[0];
+            int cols = n[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] currentRow = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+
+                    matrix[row, col] = currentRow[col];
+                }
+
+            }
+
+            int counter = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+
+                    if (matrix[row, col] == matrix[row, col + 1] && matrix[row, col] == matrix[row + 1, col] 
+                        && (matrix[row, col + 1] == matrix[row + 1, col + 1]))
+                        
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
