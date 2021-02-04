6e02cf681f2a0e78c20afcb4ed2de8a2f598d4be
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:38:58 +0200

    add

---
+++ 02. Stacks and Queues - Exercise/08.  Balanced Parentheses/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_8.__Balanced_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string leftSide = input.Substring(0, input.Length / 2);
+            string rightSide = input.Substring((input.Length / 2), input.Length / 2);
+
+            Stack<char> leftSideStack = new Stack<char>(leftSide);
+            Stack<char> rightSideStack = new Stack<char>(rightSide.Reverse());
+
+            bool isBalanced = true;
+
+            if (leftSideStack.Count < 1 && rightSideStack.Count < 1)
+            {
+                isBalanced = false;
+            }
+
+          
+
+            for (int i = 0; i < leftSide.Length; i++)
+            {
+                char currLeft = leftSideStack.Pop();
+                char currRight = rightSideStack.Pop();
+
+                if (currLeft == '(' && !(currRight == ')'))
+                {
+                    isBalanced = false;
+                }
+                else if (currLeft == '[' && !(currRight == ']'))
+                {
+                    isBalanced = false;
+                }
+                else if (currLeft == '{' && !(currRight == '}'))
+                {
+                    isBalanced = false;
+                }
+                else if (currLeft == ' ' && !(currRight == ' '))
+                {
+                    isBalanced = false;
+                }
+            }
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+        }
+    }
+}
