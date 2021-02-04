2dd6e4edc97ecac34786832a58b9de46ac23ad33
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:35:34 +0200

    add

---
+++ 01. Stacks and Queues - Lab/04. Matching Brackets/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String text = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char ch = text[i];
+
+                if (ch == '(')
+                {
+                    stack.Push(i);
+
+                }
+                else if (ch == ')')
+                {
+                    int startIndex = stack.Pop();
+
+                    string subStrings = text.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(subStrings);
+                }
+            }
+        }
+    }
+}
