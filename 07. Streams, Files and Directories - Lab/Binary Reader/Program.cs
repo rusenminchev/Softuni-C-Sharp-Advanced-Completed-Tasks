931165fbc6f5060dde9e2191885008b12be51093
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:45:09 +0200

    add

---
+++ 07. Streams, Files and Directories - Lab/Binary Reader/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.IO;
+
+namespace Binary_Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream stream = new FileStream("binary.txt", FileMode.OpenOrCreate);
+
+            using BinaryWriter binaryWriter = new BinaryWriter(stream);
+
+            binaryWriter.Write(true);
+            binaryWriter.Write(true);
+            binaryWriter.Write(false);
+            binaryWriter.Write(true);
+            binaryWriter.Write(false);
+
+            binaryWriter.Close(); 
+
+            using BinaryReader binaryReader = new BinaryReader(stream);
+
+
+            var first = binaryReader.ReadBoolean();
+            var second = binaryReader.ReadBoolean();
+
+            
+        }
+    }
+}
