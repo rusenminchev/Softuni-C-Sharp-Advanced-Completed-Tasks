1a160c9a7d2fd1719ae10b96eae78adf3003b0fd
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:45:52 +0200

    add

---
+++ 08. Streams, Files and Directories - Exercise/1. Odd Lines/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _1._Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using StreamReader reader = new StreamReader("TextFile1.txt");
+            int counter = 0;
+
+
+            while (true)
+            {
+               string line = reader.ReadLine();
+
+                if (line == null)
+                {
+                    break;
+                }
+
+                if (counter % 2 == 1)
+                {
+                    Console.WriteLine(line);
+                }
+                counter++;
+            }
+
+        }
+    }
+}
