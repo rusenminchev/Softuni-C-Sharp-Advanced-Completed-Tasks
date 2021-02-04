a754ba7d4fc5c0e3d082acb984ecd9f28d3d62cb
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:45:58 +0200

    add

---
+++ 08. Streams, Files and Directories - Exercise/2. Line Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace _2._Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using StreamReader reader = new StreamReader("TextFile1.txt");
+            using StreamWriter writer = new StreamWriter("Output.txt", true);
+
+            int counter = 1;
+
+            while (true)
+            {
+                string line = reader.ReadLine();
+
+                if (line == null)
+                {
+                    break;
+                }
+
+                // записване във файла
+                writer.WriteLine($"{counter}. {line}");
+
+                // принтиране на конзолата
+                Console.WriteLine($"{counter}. {line}");
+                counter++;
+
+                
+
+            }
+        }
+    }
+}
