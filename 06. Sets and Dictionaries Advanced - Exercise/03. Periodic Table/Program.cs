56b26668b33d020a6e50e3c072ab7aaa8b822851
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:43:50 +0200

    add

---
+++ 06. Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_3._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> uniqueEl = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+
+                    uniqueEl.Add(input[j]);
+                }
+            }
+
+            foreach (var element in uniqueEl.OrderBy(x => x))
+            {
+
+                Console.Write(element + " ");
+
+            }
+
+        }
+    }
+}
