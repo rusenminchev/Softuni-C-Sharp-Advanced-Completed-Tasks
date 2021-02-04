8712c1b3743149e01c814784043a8ef67c96fdc3
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:35:49 +0200

    add

---
+++ 01. Stacks and Queues - Lab/06. Supermarket/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input = string.Empty;
+
+            Queue<String> names = new Queue<string>();
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input != "Paid")
+                {
+                    names.Enqueue(input);
+                }
+                else
+                {
+                    Console.WriteLine(String.Join(Environment.NewLine, names));
+                    names.Clear();
+                }
+            }
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
