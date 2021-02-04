1e43c71d2112f862091e07ecb9468622c1964557
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:40:43 +0200

    add

---
+++ 03. Multidimensional Arrays - Lab/07. Pascal Triangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _07._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+
+            long[][] jaggedArray = new long[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                jaggedArray[i] = new long[i + 1];
+
+                for (int t = 0; t < i + 1; t++)
+                {
+                    if (t > 0 && t < jaggedArray[i].Length - 1)
+                    {
+                        jaggedArray[i][t] = jaggedArray[i - 1][t - 1] + jaggedArray[i - 1][t];
+                    }
+                    else
+                    {
+                        jaggedArray[i][t] = 1;
+                    }
+                }
+            }
+
+            foreach (long[] item in jaggedArray)
+            {
+                Console.WriteLine(string.Join(' ', item));
+            }
+        }
+    }
+}
