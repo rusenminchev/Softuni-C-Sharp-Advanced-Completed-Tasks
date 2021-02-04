2e2c9017b3c15e4ff6de6a777b7db53d42d8056e
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:35:41 +0200

    add

---
+++ 01. Stacks and Queues - Lab/05. Print Even Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _5._Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).Where(c => c % 2 == 0).ToArray());
+            Console.WriteLine(String.Join(", ", queue));
+
+        }
+    }
+}
