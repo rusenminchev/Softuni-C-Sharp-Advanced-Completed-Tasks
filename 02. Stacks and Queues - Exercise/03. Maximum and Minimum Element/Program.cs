cd2b8a2edba39e80195daab50d77227ec02595a0
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:38:29 +0200

    add

---
+++ 02. Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace Problem_3._Maximum_and_Minimum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<int> numbers = new Stack<int>();
+
+            int minimum = int.MaxValue;
+            int maximum = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                int queryType = input[0];
+
+                switch (queryType)
+                {
+                    case 1:
+                        int element = input[1];
+
+                        numbers.Push(element);
+
+                        break;
+                    case 2:
+                        if (numbers.Any())
+                        {
+                            numbers.Pop();
+                        }
+
+                        break;
+                    case 3:
+                        if (numbers.Any())
+                        {
+                            for (int k = 0; k < numbers.Count; k++)
+                            {
+
+                                int currentNumber = numbers.Peek();
+                                if (maximum < currentNumber)
+                                {
+                                    maximum = currentNumber;
+                                    Console.WriteLine(maximum);
+                                }
+
+                            }
+
+                        }
+
+                        break;
+
+                    case 4:
+
+                        if (numbers.Any())
+                        {
+                            for (int j = 0; j < numbers.Count; j++)
+                            {
+                                int currentNumber = numbers.Peek();
+                                if (minimum > currentNumber)
+                                {
+                                    minimum = currentNumber;
+                                    Console.WriteLine(minimum);
+                                }
+                            }
+                        }
+
+                        break;
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+    }
+}
