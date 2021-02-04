f61858831b10605a8efb54b9a810854a1a9a86df
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:40:29 +0200

    add

---
+++ 03. Multidimensional Arrays - Lab/05. Square with Maximum Sum/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace _5._Square_with_Maximum_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+            int sum = 0;
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currentRow = Console.ReadLine().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+
+                    matrix[row, col] = currentRow[col];
+                }
+
+            }
+
+            int maxSum = int.MinValue;
+            int maxRow = 0;
+            int maxCol = 0;
+
+            //за да си гарантираме, че няма да излече от границите на матрицата/ масива и да даде ексепшън въртим масива до length - 1
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    int currentSum = matrix[row, col] + matrix[row, col + 1] + matrix[row + 1, col] + matrix[row + 1, col + 1];
+
+                    // за да вземеш първата срещната или в слуая първата лява максимална стойност if-a e (currentSum > maxSum), 
+                    // при нова срещната стойност равна на вече записаната максимална, ако искаме да се ъпдейтнат стойностите, 
+                    // тогава условието на if-a става (currentSum >= maxSum)
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                        maxRow = row;
+                        maxCol = col;
+                    }
+                }
+            }
+
+            Console.WriteLine(matrix[maxRow, maxCol] + " " + matrix[maxRow, maxCol + 1]);
+            Console.WriteLine(matrix[maxRow + 1, maxCol] + " " + matrix[maxRow + 1, maxCol + 1]);
+            Console.WriteLine(maxSum);
+        }
+    }
+}
