1db98ccb5d62f6245b60cb3fe4b459831c9fba45
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:41:34 +0200

    add

---
+++ 04. Multidimensional Arrays - Exercise/05. Snake Moves/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_5._Snake_Moves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int rows = n[0];
+            int cols = n[1];
+
+            string[,] matrix = new string[rows, cols];
+
+           
+                string[] currentRow = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int col = 0; col < cols; col++)
+                { 
+
+                    matrix[0, col] = currentRow[col];
+                }
+            
+
+
+        }
+    }
+}
