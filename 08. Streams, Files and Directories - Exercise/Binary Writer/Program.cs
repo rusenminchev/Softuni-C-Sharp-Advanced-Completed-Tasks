607f9eb83a1b5eb047b43e649e9a1259d8a67816
Rusen Minchev <rusenminchev@gmail.com>
2021-02-04 21:46:20 +0200

    add

---
+++ 08. Streams, Files and Directories - Exercise/Binary Writer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Binary_Writer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
