49316c6ec682a63271a6b74358f551c69a862937
Rusen Minchev <rusenminchev@gmail.com>
2021-02-06 23:54:26 +0200

    add

---
+++ 09. Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Func_Problem_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var input = Console.ReadLine()
+                .Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(x => x + (x * 0.2))
+                .ToList();
+
+            foreach (var number in input)
+            {
+                Console.WriteLine($"{number:f2}");
+            }
+        }
+    }
+}
