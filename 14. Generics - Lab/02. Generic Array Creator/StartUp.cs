d9bc0daf293361fcae3ad9f02d50f31ca94fc8d5
Rusen Minchev <rusenminchev@gmail.com>
2021-02-07 00:01:49 +0200

    add

---
+++ 14. Generics - Lab/02. Generic Array Creator/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace GenericArrayCreator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            string[] stringArray = ArrayCreator.Create(5, "Rusen");
+            int[] intArray = ArrayCreator.Create(5, 50);
+            int count = 1;
+
+            foreach (var item in stringArray)
+            {
+
+                Console.WriteLine($"{count++}. {item}");
+
+            }
+
+            count = 1;
+
+            foreach (var item in intArray)
+            {
+
+                Console.WriteLine($"{count++}. {item}");
+
+            }
+
+        }
+    }
+}
