eb905e1a90ecb1b9ea498934bf45c62eed83f5c6
Rusen Minchev <rusenminchev@gmail.com>
2021-02-07 00:03:45 +0200

    add

---
+++ 15. Generics - Exercise/07. Tuple/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Dynamic;
+using System.Linq;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] nameAndAddress = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .ToArray();
+
+            string firstNameLastName = $"{nameAndAddress[0]} {nameAndAddress[1]}";
+            string address = nameAndAddress[2];
+
+            Tuple<string, string> myFirstTuple = new Tuple<string, string>(firstNameLastName, address);
+
+            Console.WriteLine(myFirstTuple);
+
+            string[] nameAndBeer = Console.ReadLine()
+              .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+              .ToArray();
+
+           string name = nameAndBeer[0];
+           int litersOFBeer = int.Parse(nameAndBeer[1]);
+
+            Tuple<string, int> mySecondTuple = new Tuple<string, int>(name, litersOFBeer);
+
+            Console.WriteLine(mySecondTuple);
+
+           string[] intAndDouble = Console.ReadLine()
+              .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+              .ToArray();
+
+            int firstItem = int.Parse(intAndDouble[0]);
+            double secondItem = double.Parse(intAndDouble[1]);
+
+            Tuple<int, double> myThirdTuple = new Tuple<int, double>(firstItem, secondItem);
+
+            Console.WriteLine(myThirdTuple);
+
+        }
+    }
+}
---
+++ 15. Generics - Exercise/07. Tuple/Tuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class Tuple<firstItem,secondItem>
+    {
+
+        public Tuple(firstItem firstItem, secondItem secondItem)
+        {
+            this.FirstItem = firstItem;
+            this.SecondItem = secondItem;
+        }
+
+        public firstItem FirstItem { get; set; }
+
+        public secondItem SecondItem { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstItem} -> {this.SecondItem}";
+        }
+    }
+}
