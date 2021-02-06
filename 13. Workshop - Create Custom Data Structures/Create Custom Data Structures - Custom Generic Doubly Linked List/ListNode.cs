8d525b8ddffba0b0fc37ff650094662cc18a864f
Rusen Minchev <rusenminchev@gmail.com>
2021-02-07 00:00:59 +0200

    add

---
+++ 13. Workshop - Create Custom Data Structures/Create Custom Data Structures - Custom Generic Doubly Linked List/ListNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Create_Custom_Data_Structures___Custom_Doubly_Linked_List
+{
+    public class ListNode<T>
+    {
+        public ListNode(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; set; }
+        public ListNode<T> NextNode { get; set; }
+        public ListNode<T> PreviousNode { get; set; }
+
+
+    }
+}
---
+++ 13. Workshop - Create Custom Data Structures/Create Custom Data Structures - Custom Generic Doubly Linked List/StartUp.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Create_Custom_Data_Structures___Custom_Doubly_Linked_List
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList<string> myDoublyLinkedList = new DoublyLinkedList<string>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+
+                myDoublyLinkedList.AddFirst("Rusen" + i);
+
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+
+                myDoublyLinkedList.AddLast("Rusen" + i);
+
+            }
+
+            myDoublyLinkedList.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+
+            for (int i = 1; i <= 10; i++)
+            {
+
+                myDoublyLinkedList.RemoveFirst();
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---");
+
+            myDoublyLinkedList.ForEach(x => Console.Write(x + " "));
+
+            Console.WriteLine();
+            Console.WriteLine("---");
+
+            string[] array = myDoublyLinkedList.ToArray();
+
+            Console.WriteLine();
+            Console.WriteLine("---");
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---");
+
+            Console.WriteLine(myDoublyLinkedList[5]);
+
+            Console.WriteLine();
+            Console.WriteLine("---");
+
+            foreach (var element in myDoublyLinkedList)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+}
