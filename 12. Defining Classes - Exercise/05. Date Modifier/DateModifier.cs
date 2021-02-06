364b2ca5abc058a77f18f4950a5af5ed3d3c8074
Rusen Minchev <rusenminchev@gmail.com>
2021-02-06 23:59:43 +0200

    add

---
+++ 12. Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+
+        public int GetDaysDiff(string startDate, string endDate)
+        {
+            DateTime start = Convert.ToDateTime(startDate);
+            DateTime end = Convert.ToDateTime(endDate);
+
+            int result = Math.Abs((end - start).Days);
+
+            return result;
+        }
+    }
+}
---
+++ 12. Defining Classes - Exercise/05. Date Modifier/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string startDate = Console.ReadLine();
+            string endDate = Console.ReadLine();
+
+            DateModifier dateModifire= new DateModifier();
+
+            Console.WriteLine(dateModifire.GetDaysDiff(startDate, endDate));
+
+        }
+    }
+}
