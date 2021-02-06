db366a74acf50598dccd920db655fb3e20e1321e
Rusen Minchev <rusenminchev@gmail.com>
2021-02-06 23:59:31 +0200

    add

---
+++ 12. Defining Classes - Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private HashSet<Person> people;
+
+        public Family()
+        {
+            this.people = new HashSet<Person>();
+        }
+
+
+        public void AddMamber(Person person)
+        {
+            this.people.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestPerson = this.people
+                .OrderByDescending(p=>p.Age)
+                .FirstOrDefault();
+
+            return oldestPerson;
+        }
+
+
+    }
+}
