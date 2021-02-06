0e40b5bb5c8d39b726db3f3f6d8fe1b79eeba2ab
Rusen Minchev <rusenminchev@gmail.com>
2021-02-06 23:59:55 +0200

    add

---
+++ 12. Defining Classes - Exercise/06. Speed Racing 2/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        public Car(string model, double fuelAmount, double fuelConsuptionPerKilometer)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsuptionPerKilometer = fuelConsuptionPerKilometer;
+            this.TraveledDistance = 0;
+
+        }
+
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsuptionPerKilometer { get; set; }
+        public double TraveledDistance { get; set; }
+
+        // Като променливи за извикване на метода подаваме само нужните променливи, които са извън класа. За тези, които
+        // са вътре извикваме пропъртитата на класа.
+
+        public void Drive(double amountOfKm)
+        {
+            double fuelNeeded = FuelConsuptionPerKilometer * amountOfKm;
+
+            if (this.FuelAmount - fuelNeeded >= 0)
+            {
+                FuelAmount -= fuelNeeded;
+                TraveledDistance += amountOfKm;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ 12. Defining Classes - Exercise/06. Speed Racing 2/StartUp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpeedRacing
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] carArgs = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                   .ToArray();
+                string model = carArgs[0];
+                double fuelAmount = double.Parse(carArgs[1]);
+                double fuelConsumptionPerKilometer = double.Parse(carArgs[2]);
+
+                Car car = new Car(model, fuelAmount, fuelConsumptionPerKilometer);
+                cars.Add(car);
+            }
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string model = cmdArgs[1];
+                double amountOfKm = double.Parse(cmdArgs[2]);
+
+                Car car = cars.FirstOrDefault(x => x.Model == model);
+
+                car.Drive(amountOfKm);
+
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TraveledDistance}");
+            }
+        }
+        // С този метод можем да вземем елемент, който ни трябва от колекция, като ли List<>, HarshSet<> и т.н.
+        // Работи по същия начин, като експрешъна горе -> Car car = cars.FirstOrDefault(x => x.Model == model);
+        // Или може и чрез Dictionary, както Аз реших задачата първоначално.
+        //static Car GetTheCurrentCar(List<Car> cars, string model)
+        //{
+        //    foreach (var car in cars)
+        //    {
+        //        if (car.Model == model)
+        //        {
+        //            return car;
+        //        }
+               
+        //    }
+        //    return null;
+        //}
+    }
+}
