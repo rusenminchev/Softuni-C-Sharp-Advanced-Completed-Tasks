f78dacd51ef7acd18191e608aaf8f2ee072e7466
Rusen Minchev <rusenminchev@gmail.com>
2021-02-07 00:03:14 +0200

    add

---
+++ 15. Generics - Exercise/02. Generic Box of Integer/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxofInregers
+{
+    public class Box<T>
+    {
+        private T value;
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{this.value.GetType()}: {this.value}";
+        }
+
+    }
+}
