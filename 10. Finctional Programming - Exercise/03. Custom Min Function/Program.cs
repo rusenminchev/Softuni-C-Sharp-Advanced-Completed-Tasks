6dae14c0ff65950efb5244c6f4a07cd3da8b108e
Rusen Minchev <rusenminchev@gmail.com>
2021-02-06 23:55:15 +0200

    add

---
+++ 10. Finctional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_03._Custom_Min_Function_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Func<int[], int> minFunc = nums =>
+            {
+
+                int min = int.MaxValue;
+
+                foreach (var num in numbers)
+                {
+                    if (num < min)
+                    {
+                        min = num;
+                    }
+
+                }
+
+                return min;
+
+            };
+
+            Console.WriteLine(minFunc(numbers));
+        }
+    }
+}
