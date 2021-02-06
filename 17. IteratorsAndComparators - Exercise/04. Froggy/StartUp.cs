931516503fcd9bb66058b03d5a89b076771b4a1d
Rusen Minchev <rusenminchev@gmail.com>
2021-02-07 00:05:21 +0200

    add

---
+++ 17. IteratorsAndComparators - Exercise/04. Froggy/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var stones = Console.ReadLine()
+                .Split(", " , StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Lake lake = new Lake(stones);
+
+            Console.WriteLine(String.Join(", " , lake));
+
+        }
+    }
+}
