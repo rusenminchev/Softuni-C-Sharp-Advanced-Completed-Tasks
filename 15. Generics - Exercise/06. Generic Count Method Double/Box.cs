2c45500781637fb7ea88b2a954c22d8d198befa3
Rusen Minchev <rusenminchev@gmail.com>
2021-02-07 00:03:40 +0200

    add

---
+++ 15. Generics - Exercise/06. Generic Count Method Double/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace GenericCountMethodDoubles
+{
+    public class Box<T> where T : IComparable
+    {
+
+        private List<T> elements;
+
+        public Box(List<T> value)
+        {
+            this.elements = value;
+        }
+
+        public void Swap(List<T> list, int firstIndex, int secondIndex)
+        {
+            T temp = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = temp;
+
+        }
+
+        public int CountOfTheLargerElements(List<T> elements, T elementToCompare)
+        {
+            int count = 0;
+
+            foreach (var element in elements)
+            {
+                int comparison = elementToCompare.CompareTo(element);
+
+                if (comparison < 0 )
+                { 
+                 count++;
+                }
+            }
+
+            return count;
+
+        }
+    }
+}
---
+++ 15. Generics - Exercise/06. Generic Count Method Double/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace GenericCountMethodDoubles
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<double> elements = new List<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+
+                elements.Add(input);
+            }
+
+            double elementToCompare = double.Parse(Console.ReadLine());
+
+            Box<double> box = new Box<double>(elements);
+
+            Console.WriteLine(box.CountOfTheLargerElements(elements, elementToCompare));
+        }
+    }
+}
