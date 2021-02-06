274d38424c5779a748be5a41d0b5faf218211a61
Rusen Minchev <rusenminchev@gmail.com>
2021-02-06 23:54:55 +0200

    add

---
+++ 10. Finctional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Exercise___01._Action_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> printAction = Console.WriteLine;
+
+            Console
+                .ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(printAction);
+
+            //ver. 2
+
+            string[] names = Console.ReadLine().Split();
+
+            Action<string[]> printAction2 = x => Console.WriteLine(String.Join(Environment.NewLine, x));
+
+            printAction2(names);
+        }
+    }
+}
