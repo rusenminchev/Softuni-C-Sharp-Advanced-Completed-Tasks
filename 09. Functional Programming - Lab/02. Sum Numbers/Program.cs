2700a6a6948ed5b7f9e56675558e05bde7d34c13
Rusen Minchev <rusenminchev@gmail.com>
2021-02-06 23:54:15 +0200

    add

---
+++ 09. Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Funk_Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parser = int.Parse;
+
+            List<int> input = Console.
+                ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .ToList();
+
+            Console.WriteLine(input.Count);
+            Console.WriteLine(input.Sum());
+
+        }
+    }
+}
