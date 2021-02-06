73c3921c1689938c73551c843815a8614cd407b9
Rusen Minchev <rusenminchev@gmail.com>
2021-02-07 00:05:09 +0200

    add

---
+++ 17. IteratorsAndComparators - Exercise/02. Collection/ListyIterator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collection
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> data;
+        private int count;
+
+        public ListyIterator(params T[] data)
+        {
+            this.data = new List<T>(data);
+        }
+
+
+        public bool Move()
+        {
+            this.count++;
+
+            return count < data.Count;
+        }
+
+        public void Print()
+        {
+            if (this.data.Count == 0)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(this.data[this.count]);
+            }
+            
+        }
+
+        public bool HasNext()
+        {
+            return this.count + 1 < data.Count;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in this.data)
+            {
+                yield return item;
+            }
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ 17. IteratorsAndComparators - Exercise/02. Collection/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Collection
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToArray();
+
+            ListyIterator<string> listyIterator = new ListyIterator<string>(input);
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                if (command == "Move")
+                {
+                    Console.WriteLine(listyIterator.Move());
+                }
+                else if (command == "Print")
+                {
+                    listyIterator.Print();
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(listyIterator.HasNext());
+                }
+                else if (command == "PrintAll")
+                {
+                    foreach (var item in listyIterator)
+                    {
+                        Console.Write($"{item} ");
+                    }
+                    Console.WriteLine();
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
