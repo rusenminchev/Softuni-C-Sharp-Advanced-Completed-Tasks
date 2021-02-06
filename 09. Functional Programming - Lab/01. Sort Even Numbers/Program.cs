2a90964a570f6096ab9da8f8137b404196afb512
Rusen Minchev <rusenminchev@gmail.com>
2021-02-06 23:53:54 +0200

    add

---
+++ 09. Functional Programming - Lab/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace CS_Advanced___Functional_Programming
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
