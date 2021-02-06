6162dfe01b13a9525e32611a116848ab98562db6
Rusen Minchev <rusenminchev@gmail.com>
2021-02-07 00:01:09 +0200

    add

---
+++ 13. Workshop - Create Custom Data Structures/Create Custom Data Structures - Stack/MyStack.cs
@@ -0,0 +1,86 @@
+﻿
+using Microsoft.VisualBasic;
+using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Workshop___Create_Custom_Data_Structures
+{
+    public class MyStack
+    {
+        private int[] data;
+        private int initialCapacity = 4;
+
+
+        public MyStack()
+        {
+            this.Count = 0;
+            this.data = new int[initialCapacity];
+        }
+
+
+        public int Count { get; private set; }
+
+
+        public void Push(int element)
+        {
+            if (this.data.Length == this.Count)
+            {
+                Resize();
+            }
+            this.data[Count] = element;
+            this.Count++;
+
+        }
+
+        public int Pop()
+        {
+            ValidateStack();
+
+            int lastElement = this.data[this.Count - 1];
+            Count--;
+            return lastElement;
+        }
+
+        public int Peek()
+        {
+            ValidateStack();
+            return this.data[this.Count - 1];
+        }
+
+        
+
+        public void ForEach(Action<int> action)
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                action(this.data[i]);
+            }
+        }
+
+        private void ValidateStack()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidAsynchronousStateException("The stack is empty");
+            }
+        }
+
+        private void Resize()
+        {
+            int newCapacity = this.data.Length * 2;
+
+            var newData = new int[newCapacity];
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                newData[i] = this.data[i];
+            }
+
+            this.data = newData;
+        }
+
+        
+    }
+}
+
---
+++ 13. Workshop - Create Custom Data Structures/Create Custom Data Structures - Stack/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Workshop___Create_Custom_Data_Structures;
+
+namespace Create_Custom_Data_Structures___Stack
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MyStack myStack = new MyStack();
+
+            Console.WriteLine("Let`s push some elements in our custom stack");
+
+            myStack.Push(0);
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            myStack.Push(4);
+            myStack.Push(5);
+            myStack.Push(6);
+            myStack.Push(7);
+            myStack.Push(8);
+            myStack.Push(9);
+
+            Console.WriteLine("Let`s check the count of the stack");
+
+            Console.WriteLine(myStack.Count);
+
+            Console.WriteLine("It`s time to Pop() a few elements");
+
+            myStack.Pop();
+            myStack.Pop();
+            myStack.Pop();
+
+            Console.WriteLine("Let`s check the count again");
+
+            Console.WriteLine(myStack.Count);
+
+            Console.WriteLine("Is the Peek() method working?");
+
+            Console.WriteLine(myStack.Peek());
+
+            Console.WriteLine("Hell yeah!");
+
+            Console.WriteLine("Let`s implement ForEach() loop and apply an action to the elements in the stack");
+
+            Action<int> action = x => Console.WriteLine(x * 10);
+
+            myStack.ForEach(action);
+
+            Console.WriteLine(myStack.Count);
+
+
+
+        }
+    }
+}
