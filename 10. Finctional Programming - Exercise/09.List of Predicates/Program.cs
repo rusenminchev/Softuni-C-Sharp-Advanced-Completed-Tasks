3aef7ace97debae9f9f1c8ef1936cc1cfb167e3b
Rusen Minchev <rusenminchev@gmail.com>
2021-02-06 23:56:31 +0200

    add

---
+++ 10. Finctional Programming - Exercise/09.List of Predicates/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_09._List_Of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> deviders = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> devidibleNumbers = new List<int>();
+
+            for (int j = 1; j <= n; j++)
+            {
+                bool isDevideble = true;
+
+                foreach (var devider in deviders)
+                {
+                    Func<int, bool> devidebleFunc = x => x % devider == 0;
+
+                    if (!devidebleFunc(j))
+                    {
+                        isDevideble = false; 
+                    }
+                }
+
+                if (isDevideble)
+                {
+                    devidibleNumbers.Add(j);
+                }
+            }
+
+            Console.WriteLine(String.Join(' ', devidibleNumbers));
+        }
+    }
+}
