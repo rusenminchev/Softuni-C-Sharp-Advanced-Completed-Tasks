e529850d2d8a7b780484e3cee2bcb2d92960b01d
Rusen Minchev <rusenminchev@gmail.com>
2021-02-06 23:55:20 +0200

    add

---
+++ 10. Finctional Programming - Exercise/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_04._Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ranges = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            string filter = Console.ReadLine();
+
+            Predicate<int> predicate = filter == "odd" ?
+                new Predicate<int>(n => n % 2 != 0) : new
+                Predicate<int>(n => n % 2 == 0);
+
+            List<int> result = new List<int>();
+
+            for (int i = ranges[0]; i <= ranges[1]; i++)
+            {
+                if (predicate(i))
+                {
+                    result.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
