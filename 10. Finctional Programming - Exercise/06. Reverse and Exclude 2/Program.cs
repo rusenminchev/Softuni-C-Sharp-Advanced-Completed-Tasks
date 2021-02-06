bc242076d9beaccfff07a3b27b70b9559dc44092
Rusen Minchev <rusenminchev@gmail.com>
2021-02-06 23:56:13 +0200

    add

---
+++ 10. Finctional Programming - Exercise/06. Reverse and Exclude 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_06._Reverse_and_Exclude_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).Reverse().ToList();
+            int devider = int.Parse(Console.ReadLine());
+
+            Func<int, bool> predicate = x => x % devider != 0;
+
+            numbers = numbers.Where(predicate).ToList();
+            //numbers = numbers.Where(x => x % devider != 0).ToList();
+
+            Console.WriteLine(String.Join(' ', numbers));
+
+        }
+    }
+}
