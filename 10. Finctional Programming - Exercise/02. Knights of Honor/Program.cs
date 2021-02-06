1a11e6839a6eabe255fcc05a63f6c1c2654ec58c
Rusen Minchev <rusenminchev@gmail.com>
2021-02-06 23:55:10 +0200

    add

---
+++ 10. Finctional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Exercise___02._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> names = Console.ReadLine().Split().ToList();
+            // names.Select(x => $"Sir {x}");
+            names = mySelect(names, x => $"Sir {x}");
+
+            Action<List<string>> printNames = x => Console.WriteLine(String.Join(Environment.NewLine, x));
+            printNames(names);
+        }
+
+        static List<string> mySelect(List<string> list, Func<string, string> myFunc)
+        {
+
+            List<string> newList = new List<string>();
+
+
+            foreach (var item in list)
+            {
+
+                string newItem = myFunc(item);
+                newList.Add(newItem);
+
+            }
+
+            return newList;
+        }
+    }
+}
