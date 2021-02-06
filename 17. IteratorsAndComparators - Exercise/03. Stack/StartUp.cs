8e3afcb5d57c093379b8c88be6f698440644c1c4
Rusen Minchev <rusenminchev@gmail.com>
2021-02-07 00:05:15 +0200

    add

---
+++ 17. IteratorsAndComparators - Exercise/03. Stack/StartUp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Stack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            string command = Console.ReadLine();
+
+            myStack<int> myStack = new myStack<int>();
+
+            while (command !="END")
+            {
+                if (command.Contains("Push"))
+                {
+                    var input = command
+                        .Split(new[] { " ", ", " }, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .Skip(1)
+                        .ToList();
+
+                    for (int i = 0; i < input.Count; i++)
+                    {
+                        myStack.Push(input[i]);
+                    }
+
+                }
+                else if (command.Contains("Pop"))
+                {
+                    try
+                    {
+                        myStack.Pop();
+                    }
+                    catch(InvalidOperationException exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }         
+                }
+
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var element in myStack)
+            {
+                Console.WriteLine(element);
+            }
+             foreach (var element in myStack)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+}
---
+++ 17. IteratorsAndComparators - Exercise/03. Stack/myStack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+   public class myStack<T> : IEnumerable<T>
+    {
+        private List<T> data;
+
+        public myStack()
+        {
+            this.data = new List<T>();
+        }
+
+        public void Push(T element)
+        {
+            this.data.Add(element);
+        }
+
+        public void Pop()
+        {
+            if (this.data.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+            
+                this.data.Remove(this.data[this.data.Count - 1]);
+            
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.data.Count - 1; i >= 0; i--)
+            {
+                yield return this.data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
