83f62869735d31ce93a089dca0340fc0f7f940b3
Rusen Minchev <rusenminchev@gmail.com>
2021-02-07 00:03:33 +0200

    add

---
+++ 15. Generics - Exercise/05. Generic Count Method String/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace GenericCountMethodStrings
+{
+    public class Box<T> where T : IComparable
+    {
+
+        private List<T> elements;
+
+        public Box(List<T> elements)
+        {
+            this.elements = elements;
+        }
+
+        public void Swap(List<T> list, int firstIndex, int secondIndex)
+        {
+            T temp = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = temp;
+
+        }
+
+        public int CounterOfLargerElements(List<T> elements, T elementToCompare)
+        {
+            int count = 0;
+
+            foreach (var element in elements)
+            {
+                int comparison = elementToCompare.CompareTo(element);
+
+                if (comparison < 0 )
+                { 
+                 count++;
+                }
+            }
+
+            return count;
+
+        }
+    }
+}
---
+++ 15. Generics - Exercise/05. Generic Count Method String/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace GenericCountMethodStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> elements = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                elements.Add(input);
+            }
+
+            string elementToCompare = Console.ReadLine();
+
+            Box<string> box = new Box<string>(elements);
+
+            Console.WriteLine(box.CounterOfLargerElements(elements, elementToCompare));
+        }
+    }
+}
