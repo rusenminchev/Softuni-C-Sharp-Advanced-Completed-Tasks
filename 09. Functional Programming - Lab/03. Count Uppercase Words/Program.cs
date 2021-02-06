8e86d9947d1b7cc0d05702a71b590ce8b4538644
Rusen Minchev <rusenminchev@gmail.com>
2021-02-06 23:54:21 +0200

    add

---
+++ 09. Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Func_Problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> checker = input => char.IsUpper(input[0]);
+
+            Console.ReadLine()
+                 .Split()
+                 .Where(checker)
+                 .ToList()
+                 .ForEach(Console.WriteLine);
+
+        }
+    }
+}
