2eb390ad2f73d6505c07337af7c3146b7e11ed20
Rusen Minchev <rusenminchev@gmail.com>
2021-02-07 00:01:43 +0200

    add

---
+++ 14. Generics - Lab/01. Box Of T/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+
+        private List<T> data;
+
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.data.Add(element);
+        }
+
+        public T Remove()
+        {
+
+            var result = this.data[data.Count - 1];
+            data.Remove(data[data.Count - 1]);
+            return result;
+        }
+    }
+}
