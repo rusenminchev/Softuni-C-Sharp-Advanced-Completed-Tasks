f28185e26ae0d8f9c7998cbbed0dfc88fbfc8d71
Rusen Minchev <rusenminchev@gmail.com>
2021-02-06 23:56:19 +0200

    add

---
+++ 10. Finctional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7._Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Where(x => x.Length <= n).ToList()
+                .ForEach(Console.WriteLine);
+
+            //names = names.Where(x => x.Length <= 0).ToList();
+
+
+            //Func<List<string>, List<string>> isItTheRightLenghtFunc = names =>
+            //{
+            //    List<string> theRightLenghtList = new List<string>();
+
+            //    foreach (var name in names)
+            //    {
+            //        if (name.Length <= n)
+            //        {
+            //            theRightLenghtList.Add(name);
+            //        }
+            //    }
+
+            //    return theRightLenghtList;
+            //};
+
+            //Console.WriteLine(String.Join(Environment.NewLine, isItTheRightLenghtFunc(names)));    
+
+        }
+    }
+}
