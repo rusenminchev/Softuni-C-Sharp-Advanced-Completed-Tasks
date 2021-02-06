8e0d5803b7f404436b3a872797119905ccd62e69
Rusen Minchev <rusenminchev@gmail.com>
2021-02-07 00:01:15 +0200

    add

---
+++ 13. Workshop - Create Custom Data Structures/Workshop - Create Custom Data Structures/MyList.cs
@@ -0,0 +1,153 @@
+﻿
+using Microsoft.VisualBasic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Workshop___Create_Custom_Data_Structures
+{
+    public class MyList
+    {
+        private int[] data;
+        private int initialCapacity = 4;
+
+        public MyList()
+        {
+            this.data = new int[initialCapacity];
+        }
+
+
+        public int Count { get; private set; } = 0;
+
+        public int this[int index]
+        {
+            get
+            {
+                IndexValidation(index);
+                return data[index];
+            }
+            set
+            {
+                IndexValidation(index);
+                data[index] = value;
+            }
+        }
+
+        public void Add(int element)
+        {
+            if (this.data.Length == this.Count)
+            {
+                Resize();
+            }
+                this.data[Count] = element;
+                this.Count++;
+            
+        }
+
+        public int RemoveAt(int index)
+        {
+
+            var itemToRemove = this.data[index];
+
+            IndexValidation(index);
+
+            ShiftsElementsWhenRemove(index);
+            Count--;
+
+            if (this.Count <= this.data.Length / 4)
+            {
+               Shrink();
+            }
+
+            return itemToRemove;
+        }
+        
+        public void InsertAt(int index, int element)
+        {
+            IndexValidation(index);
+
+            if (this.data.Length == this.Count)
+            {
+                Resize();
+            }
+
+            ShiftElementsWhenInsert(index);
+
+            this.data[index] = element;
+            Count++;
+        }
+
+        public bool Contains(int element)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this.data[i] == element)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            IndexValidation(firstIndex);
+            IndexValidation(secondIndex);
+
+            int temp = this.data[firstIndex];
+            this.data[firstIndex] = this.data[secondIndex];
+            this.data[secondIndex] = temp;
+        }
+
+        private void IndexValidation(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new System.Exception($"The index is out of range. The index must be in range from 0 to {Count - 1}");
+            }
+        }
+
+        private void Resize()
+            {
+                int newCapacity = this.data.Length * 2;
+
+                var newData = new int[newCapacity];
+
+                for (int i = 0; i < this.Count; i++)
+                {
+                    newData[i] = this.data[i];
+                }
+
+                this.data = newData;
+            }
+        private void Shrink()
+        {
+            int newCapacity = this.data.Length / 2;
+
+            var newData = new int[newCapacity];
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                newData[i] = this.data[i];
+            }
+
+            this.data = newData;
+        }
+        private void ShiftsElementsWhenRemove(int index)
+        {
+            for (int i = index; i < this.Count; i++)
+            {
+                data[i] = data[i + 1];
+            }
+        }
+
+        private void ShiftElementsWhenInsert(int index)
+        {
+            for (int i = Count - 1; i >= index; i--)
+            {
+                data[i + 1] = data[i];
+            }
+        }
+    }
+    }
+
---
+++ 13. Workshop - Create Custom Data Structures/Workshop - Create Custom Data Structures/StartUp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Workshop___Create_Custom_Data_Structures
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MyList myList = new MyList();
+
+            Console.WriteLine("Let`s add some elements");
+
+            for (int i = 0; i < 10; i++)
+            {
+                myList.Add(i);
+            }
+
+            Console.WriteLine("Let`s see what we have just added");
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+
+            Console.WriteLine("Let`s remove something");
+
+            var result = myList.RemoveAt(5);
+
+
+            Console.WriteLine($"We removed this elements: {result}");
+
+            result = myList.RemoveAt(6);
+
+            Console.WriteLine($"We removed this elements: {result}");
+
+            Console.WriteLine("Let`s see what`s up in our list");
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+
+            Console.WriteLine("Let`s check is it contains some different elements");
+
+            Console.WriteLine(myList.Contains(11));
+            Console.WriteLine(myList.Contains(5));
+            Console.WriteLine(myList.Contains(3));
+
+            Console.WriteLine("It`s time to try out Swap method");
+
+            myList.Swap(0, 3);
+
+
+            Console.WriteLine(myList[0]);
+            Console.WriteLine(myList[3]);
+
+            Console.WriteLine("Yeah! It works!");
+
+            Console.WriteLine("Let`s insert the element 33 at index 3");
+
+            myList.InsertAt(3, 33);
+
+            Console.WriteLine("Let`s see what`s happening");
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+
+            Console.WriteLine("Let`s remove most of the elements to test the Shrink() method");
+
+            myList.RemoveAt(0);
+            myList.RemoveAt(0);
+            myList.RemoveAt(0);
+            myList.RemoveAt(0);
+            myList.RemoveAt(0);
+            myList.RemoveAt(0);
+            myList.RemoveAt(0);
+            myList.RemoveAt(0);
+
+            Console.WriteLine($"Final count is: {myList.Count}");
+
+        }
+    }
+}
