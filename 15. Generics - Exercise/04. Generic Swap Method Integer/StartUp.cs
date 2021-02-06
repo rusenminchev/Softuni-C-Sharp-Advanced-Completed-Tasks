7bf99aac1e9f9a3f7b01ea90f7317ef9b12a1013
Rusen Minchev <rusenminchev@gmail.com>
2021-02-07 00:03:27 +0200

    add

---
+++ 15. Generics - Exercise/04. Generic Swap Method Integer/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace GenericSwapMethodIntegers
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                list.Add(input);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int firstIndex = indexes[0];
+            int secondIndex = indexes[1];
+
+
+            Box<int> box = new Box<int>(list);
+
+           box.Swap(list, firstIndex, secondIndex);
+
+            Console.WriteLine(box);
+        }
+    }
+}
